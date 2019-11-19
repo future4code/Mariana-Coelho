32189cc955fe0147af5ac3cb3db212800d3d883b
coelhomariana <coelhomariana@outlook.com>
2019-11-19 16:01:13 -0300

    atualizei com o comando para apagar o input

--- semana4/aula2-objetos-string/cadastro-alunos/script.js
+++ semana4/aula2-objetos-string/cadastro-alunos/script.js
@@ -11,5 +11,9 @@ function aoClicarEmCadastrar() {
 
     const adicionarNovoInscrito = document.getElementById("inscritos")
     adicionarNovoInscrito.innerHTML += "<div id='cadaInscrito'>" + "<p>" + inscrito.nome + "</p>" + "<p>" + inscrito.idade + "</p>" + "<p>" + inscrito.email + "</p>" + "</div>"
+    //Para apagar o input
+    document.getElementById('input-nome').value = ""
+    document.getElementById('input-idade').value = ""
+    document.getElementById('input-email').value = ""
 
-}
+}  
