946abf48e345d9cfd6a44a215aa4c20201277555
coelhomariana <coelhomariana@outlook.com>
2020-02-10 16:25:50 -0300

    Alteração pra pula a linha ao adicionar a nova tarefa

--- semana14/aula1/gestorDeTarefas/gestorDeTarefas.js
+++ semana14/aula1/gestorDeTarefas/gestorDeTarefas.js
@@ -3,8 +3,8 @@ const fileName = process.argv[2];
 const data = process.argv[3];
 
 try{
-    fs.appendFileSync(fileName, data, 'utf8')
+     fs.appendFileSync(fileName,"\n" + data, 'utf8');
     console.log("Dado inserido com sucesso!")
 }catch(err){
     console.log("Erro");
-}
+} 
