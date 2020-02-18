9c0398bd8b88dbbc80a97447c4aa06e1349754bc
coelhomariana <coelhomariana@outlook.com>
2020-02-18 16:41:56 -0300

    Inicio do exercicio 1

---
+++ semana15/aula2/src/index.ts
@@ -0,0 +1,6 @@
+import { SaltyDish } from './saltyDish'
+import { Dessert } from './dessert'
+
+const carbonara = new SaltyDish(50, 10, ['macarrao'], 20)
+
+const brigadeiro = new Dessert(5, 1, ['chocolate', 'leite condensado'], 30, 10)
