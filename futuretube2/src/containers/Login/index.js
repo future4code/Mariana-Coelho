91601fffa6fb2d0fa1af0c860268c33add9d0239
coelhomariana <coelhomariana@outlook.com>
2020-04-07 20:17:03 -0300

    Criadas algumas pastas do redux

---
+++ futuretube2/src/containers/Login/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+function Login(props) {
+    return (
+        <div>Página de login</div>
+    )
+        
+}
+
+
+export default Login;
