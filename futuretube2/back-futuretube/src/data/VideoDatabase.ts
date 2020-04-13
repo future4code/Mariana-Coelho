58bf82241872aad46d9a823f464fdb8652696f49
coelhomariana <coelhomariana@outlook.com>
2020-04-13 14:11:38 -0300

    Arquivo que ficou faltando dar add

--- futuretube2/back-futuretube/src/data/VideoDatabase.ts
+++ futuretube2/back-futuretube/src/data/VideoDatabase.ts
@@ -1,7 +1,8 @@
 import MainDB from "./MainDatabase";
 import Video from "../business/entities/Video";
+import VideoGateway from "../business/gateways/VideoGateway";
 
-export default class VideoDB extends MainDB {
+export default class VideoDB extends MainDB implements VideoGateway {
     async uploadVideo(video: Video){
         try{
             await this.connection.raw(`INSERT into videos values('${video.getId()}','1','${video.getUrl()}','${video.getTitle()}','${video.getDescription()}')`)
